3169d6eb6bca270af3a60948ea4f34f4a430622c
Elvia Attack <elviaatt@ga-m5.msk.21-school.ru>
2023-01-19 23:55:19 +0300

    develop/part5 - answer 'yes' is removed

--- copyToProduction.sh
+++ copyToProduction.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/expect
 
 spawn scp src/cat/s21_cat user1@10.10.0.1:s21_cat
-expect "Are you sure you want to continue connecting (yes/no/\[fingerprint\])?"
-send "yes\r"
 expect "user1@10.10.0.1's password:"
 send "555\r"
+# expect "Are you sure you want to continue connecting (yes/no/\[fingerprint\])?"
+# send "yes\r"
 
 spawn scp src/grep/s21_grep user1@10.10.0.1:s21_grep
-expect "Are you sure you want to continue connecting (yes/no/\[fingerprint\])?"
-send "yes\r"
 expect "user1@10.10.0.1's password:"
 send "555\r"
+# expect "Are you sure you want to continue connecting (yes/no/\[fingerprint\])?"
+# send "yes\r"
 
 spawn ssh -t user1@10.10.0.1 "sh -c 'sudo mv s21_cat /usr/local/bin/' && sh -c 'sudo mv s21_grep /usr/local/bin/'"
-expect "Are you sure you want to continue connecting (yes/no/\[fingerprint\])?"
-send "yes\r"
+# expect "Are you sure you want to continue connecting (yes/no/\[fingerprint\])?"
+# send "yes\r"
 expect "user1@10.10.0.1's password:"
 send "555\r"
 expect "password for user1:"
