f9c21044d74735f71fe7e8d472282061f55ac5ac
Elvia Attack <elviaatt@ga-m5.msk.21-school.ru>
2023-01-19 23:33:58 +0300

    develop/part5 - copying script worked from vm

---
+++ copyToProduction.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/expect
+
+spawn scp src/cat/s21_cat user1@10.10.0.1:s21_cat
+expect "user1@10.10.0.1's password:"
+send "555\r"
+
+spawn scp src/grep/s21_grep user1@10.10.0.1:s21_grep
+expect "user1@10.10.0.1's password:"
+send "555\r"
+
+spawn ssh -t user1@10.10.0.1 "sh -c 'sudo mv s21_cat /usr/local/bin/' && sh -c 'sudo mv s21_grep /usr/local/bin/'"
+expect "user1@10.10.0.1's password:"
+send "555\r"
+expect "password for user1:"
+send "555\r"
