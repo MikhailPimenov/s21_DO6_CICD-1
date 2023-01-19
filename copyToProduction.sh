0c4dc2a15ad61891322e2904e648881637f3b407
Elvia Attack <elviaatt@ga-m5.msk.21-school.ru>
2023-01-20 01:02:13 +0300

    develop/part5 - copying script testing 4

--- copyToProduction.sh
+++ copyToProduction.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/expect
 
-spawn scp src/cat/s21_cat user1@10.10.0.1:s21_cat
-expect "user1@10.10.0.1's password:"
-send "555\r"
+# spawn scp src/cat/s21_cat user1@10.10.0.1:s21_cat
+# expect "user1@10.10.0.1's password:"
+# send "555\r"
+
 # expect "Are you sure you want to continue connecting (yes/no/\[fingerprint\])?"
 # send "yes\r"
 
-spawn scp src/grep/s21_grep user1@10.10.0.1:s21_grep
-expect "user1@10.10.0.1's password:"
-send "555\r"
+
+# spawn scp src/grep/s21_grep user1@10.10.0.1:s21_grep
+# expect "user1@10.10.0.1's password:"
+# send "555\r"
+
 # expect "Are you sure you want to continue connecting (yes/no/\[fingerprint\])?"
 # send "yes\r"
+
+
 
 # spawn ssh user1@10.10.0.1 "sh -c 'mv s21_cat /usr/local/bin/' && sh -c 'mv s21_grep /usr/local/bin/'"
 # spawn ssh -t user1@10.10.0.1 "sh -c 'sudo mv s21_cat /usr/local/bin/' && sh -c 'sudo mv s21_grep /usr/local/bin/'"
