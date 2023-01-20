c37cd8bed0e89a9a23cbe8a3deac9c282ff98d25
Elvia Attack <elviaatt@ga-m5.msk.21-school.ru>
2023-01-20 23:03:45 +0300

    develop/part5 - really working script with error codes 23

--- copyToProduction.sh
+++ copyToProduction.sh
@@ -30,10 +30,11 @@ expect {
     "assword for $productionUser:" {    # sending password to execute sudo command on remote machine
         puts "sending password for sudo"
         send "$productionPassword\r"
-        expect eof
-        puts "changing timeout"
-        set timeout 0
-        exit 0                          # good exit. We have input password after command had reqiured it and have made sure this command to finish
+        expect eof {
+            puts "changing timeout"
+            set timeout 0
+            exit 0                          # good exit. We have input password after command had reqiured it and have made sure this command to finish
+        }
     }
 
     "$lastWordsFromPreviousOutput" {    # after the connection is established commands are executed on remote machine
