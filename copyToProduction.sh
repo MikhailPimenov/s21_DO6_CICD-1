05e24b050b9185410ad459a966cb38ce1d14ab2d
Elvia Attack <elviaatt@ga-m5.msk.21-school.ru>
2023-01-20 23:55:31 +0300

    develop/part5 - really working script with error codes 25

--- copyToProduction.sh
+++ copyToProduction.sh
@@ -4,7 +4,7 @@ set productionUser "user1"
 set productionPassword "555"
 set productionAddress "10.10.0.1"
 
-set lastWordsFromPreviousOutput "from 10.10.0.2"
+set lastWordsFromPreviousOutput "from 10.10.0.2"    # find it out mannually, what is the last line and the last words
 
 spawn ssh -l $productionUser $productionAddress
 
@@ -13,21 +13,21 @@ set timeout 120
 expect {
     timeout {
         puts "Connection timed out"
-        exit 1                          # bad exit. Time is over end we didn't succeed
+        exit 1                              # bad exit. Time is over end script was not executed
     }
 
-    "yes/no" {                          # when connecting for the very first time
+    "yes/no" {                              # when connecting for the very first time
         send "yes\r"
         exp_continue
     }
 
-    "assword:" {                        # sending password to establish connection
+    "assword:" {                            # sending password to establish connection
         puts "sending password"
         send "$productionPassword\r"
         exp_continue
     }
 
-    "assword for $productionUser:" {    # sending password to execute sudo command on remote machine
+    "assword for $productionUser:" {        # sending password to execute sudo command on remote machine
         puts "sending password for sudo"
         
         send "$productionPassword\r"
@@ -35,7 +35,7 @@ expect {
         expect {
             "$ " {
                 puts "success"
-                exit 0                   # good exit. We have input password after command had reqiured it and have made sure this command \to finish
+                exit 0                      # good exit. We have input password after command had reqiured it and have made sure this command \to finish
             }                         
             "File exists" {
                 puts "File exists. Exiting"
@@ -44,7 +44,7 @@ expect {
         }
     }
 
-    "$lastWordsFromPreviousOutput" {    # after the connection is established commands are executed on remote machine
+    "$lastWordsFromPreviousOutput" {        # after the connection is established commands are executed on remote machine
         puts "mkdir folderrrr!!!!!"
         send "sudo mkdir folder\r"
 
