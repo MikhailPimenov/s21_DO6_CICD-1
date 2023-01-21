1e92d8521771042a86c3f41fbde8081c7fa79bd0
Elvia Attack <elviaatt@ga-m5.msk.21-school.ru>
2023-01-21 20:01:47 +0300

    develop/part6 - notify script in progress 2

--- notify.sh
+++ notify.sh
@@ -7,10 +7,12 @@ TELEGRAM_BOT_TOKEN=5975522929:AAHuxcWCXDfAIwtSJ6exdDy6-ynu97WLcVg
 # STAGE_NAME=$1
 # STAGE_STATUS=$2
 # STAGE_URL=$3
+# $CI_PROJECT_NAME
+MY_CI_PROJECT_NAME='elviaatt DO6 CI/CD'
 
 TIME="10"
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-TEXT="Deploy status: $1%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
+TEXT="Stage: $CI_JOB_STAGE%0AJob: $CI_JOB_NAME%0AStatus: $CI_JOB_STATUS%0A%0AProject:+$MY_CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
 # TEXT="You fucking cocksucker! Holy shit, motherfuckers"
 
 
