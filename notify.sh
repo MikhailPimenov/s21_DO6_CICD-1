6fe8b5e0c6052aa10b06f07100fc3090f3033317
Elvia Attack <elviaatt@ga-m5.msk.21-school.ru>
2023-01-21 20:34:50 +0300

    develop/part6 notification format testing 3

--- notify.sh
+++ notify.sh
@@ -2,22 +2,15 @@
 
 #  user id. Can be obtained using @GetMyIdBot
 TELEGRAM_USER_ID=722828510
+#  user id. Is obtained when creating bot via @BotFather
 TELEGRAM_BOT_TOKEN=5975522929:AAHuxcWCXDfAIwtSJ6exdDy6-ynu97WLcVg  
 
-# STAGE_NAME=$1
-# STAGE_STATUS=$2
-# STAGE_URL=$3
-# $CI_PROJECT_NAME
 MY_CI_PROJECT_NAME='elviaatt DO6 CI/CD'
 
 TIME="10"
 URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-TEXT="Status: $CI_JOB_STATUS%0APipeline: $CI_PIPELINE_IID%0AStage: $CI_JOB_STAGE%0AJob: $CI_JOB_NAME%0A%0AProject:+$MY_CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
-# TEXT="You fucking cocksucker! Holy shit, motherfuckers"
+TEXT="Status: $CI_JOB_STATUS%0APipeline: $CI_PIPELINE_IID%0AStage: $CI_JOB_STAGE%0AJob: $CI_JOB_NAME%0A%0AProject:+$MY_CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG%0AMessage:$CI_COMMIT_MESSAGE"
 
-
-
-# TEXT="STAGE: $STAGE_NAME%0A%0ASTATUS:+$STATUS%0AURL:+$STAGE_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
 
 
 curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
